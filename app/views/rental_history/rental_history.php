bc7a6d5075559af4b8344f07e733042060b4d0d2
NewmanM <m_tanel@live.ca>
2017-03-29 22:52:56 -0400

    messed up before, back in it now. Rentalhistory all good

--- app/views/rental_history/rental_history.php
+++ app/views/rental_history/rental_history.php
@@ -1,43 +1,28 @@
 <!-- view for the sign-in form -->
 <div class="container">
-	<div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-		<div class="panel panel-info">
-			<div class="panel-heading">
-				<div class="panel-title">Sign In</div>
-			</div>
-
-			<div style="padding-top:30px" class="panel-body">
-
-				<div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
-				<!-- sign in form -->
-				<form id="sign-in-form" class="form-horizontal" role="form" action="" method="POST">
-					<!-- username field -->
-					<div style="margin-bottom: 25px" class="input-group col-sm-12 controls">
-						<input id="login-username" type="text" class="form-control" name="username" value="" placeholder="username">
-					</div>
-					<!-- password field -->
-					<div style="margin-bottom: 25px" class="input-group col-sm-12 controls">
-						<input id="login-password" type="password" class="form-control" name="password" placeholder="password">
-					</div>
-					<!-- Button -->
-					<div style="margin-top:10px" class="form-group">
-						<div class="col-sm-12 controls">
-							<input type="submit" class="btn btn-info btn-block" id="btn-login">
-						</div>
-					</div>
-					
-					<div class="form-group">
-						<div class="col-md-12 control">
-							<div style="border-top: 1px solid#888; padding-top:15px; font-size:85%">
-								Don't have an account?
-								<a href="?controller=register&action=getViewRegister">
-									Register Here
-								</a>
-							</div>
-						</div>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
+	<?php
+		$table = "<table border=1><tr>";
+		$table .= "<th>Vin</th>";
+		$table .= "<th>Rent Date</th>";
+		$table .= "<th>Pick Up Reading</th>";
+		$table .= "<th>Drop Off Reading</th>";
+		$table .= "<th>Status</th>";
+		$table .= "<th>Pick Up Time</th>";
+		$table .= "<th>Drop Off Time</th>";
+		$table .= "</tr>";
+		// Data still being returned, builds a table as a string
+		foreach ($history as $row) {
+				$table .= "<tr>";
+				$table .= "<td> " . $row["vin"] . " </td>";
+				$table .= "<td> " . $row["rent_date"] . " </td>";
+				$table .= "<td> " . $row["pick_up_odometer_reading"] . " </td>";
+				$table .= "<td> " . $row["drop_off_odometer_reading"] . " </td>";
+				$table .= "<td> " . $row["status"] . " </td>";
+				$table .= "<td> " . $row["pick_up_time"] . " </td>";
+				$table .= "<td> " . $row["drop_off_time"] . " </td>";
+				$table .= "</tr>";
+		}
+		$table .= "</table>";
+		echo $table;
+	?>
 </div>
