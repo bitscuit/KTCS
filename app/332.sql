a5ad7448e506c0ae2f165722ce9548628b7cc179
NewmanM <m_tanel@live.ca>
2017-03-29 22:45:28 -0400

    changed reservation table columns

--- app/332.sql
+++ app/332.sql
@@ -1,164 +1,120 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.1.19-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win32
--- HeidiSQL Version:             9.4.0.5125
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
-
--- Dumping database structure for 332
-CREATE DATABASE IF NOT EXISTS `332` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `332`;
-
--- Dumping structure for table 332.car
-CREATE TABLE IF NOT EXISTS `car` (
-  `vin` varchar(20) NOT NULL,
-  `parking_address` varchar(50) NOT NULL,
-  `make` varchar(50) NOT NULL,
-  `model` varchar(50) NOT NULL,
-  `make_year` year(4) NOT NULL,
-  `daily_rental_fee` decimal(15,2) NOT NULL,
-  PRIMARY KEY (`vin`),
-  KEY `FK_car_parking_location` (`parking_address`),
-  CONSTRAINT `FK_car_parking_location` FOREIGN KEY (`parking_address`) REFERENCES `parking_location` (`parking_address`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Dumping data for table 332.car: ~2 rows (approximately)
-/*!40000 ALTER TABLE `car` DISABLE KEYS */;
-INSERT INTO `car` (`vin`, `parking_address`, `make`, `model`, `make_year`, `daily_rental_fee`) VALUES
-	('123gh45rt7y6534rw', '67 Albert St', 'Chevy', 'Cruise', '2016', 105.00),
-	('736hfyst618hye76t', '20 Division St', 'Ferrari', 'Spider', '1996', 600.99);
-/*!40000 ALTER TABLE `car` ENABLE KEYS */;
-
--- Dumping structure for table 332.car_maintenance_history
-CREATE TABLE IF NOT EXISTS `car_maintenance_history` (
-  `vin` varchar(20) NOT NULL,
-  `maintenance_date` date NOT NULL,
-  `odometer_reading` int(20) NOT NULL,
-  `maintenance_type` enum('Scheduled','Repair','Body Work') NOT NULL,
-  `description` varchar(50) NOT NULL,
-  `maintenance_time` time NOT NULL,
-  PRIMARY KEY (`maintenance_date`,`vin`),
-  KEY `FK_car_maintenance_history_car` (`vin`),
-  CONSTRAINT `FK_car_maintenance_history_car` FOREIGN KEY (`vin`) REFERENCES `car` (`vin`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Dumping data for table 332.car_maintenance_history: ~2 rows (approximately)
-/*!40000 ALTER TABLE `car_maintenance_history` DISABLE KEYS */;
-INSERT INTO `car_maintenance_history` (`vin`, `maintenance_date`, `odometer_reading`, `maintenance_type`, `description`, `maintenance_time`) VALUES
-	('736hfyst618hye76t', '2017-02-02', 2789, 'Scheduled', 'Oil Change', '12:16:45'),
-	('123gh45rt7y6534rw', '2017-02-17', 20176, 'Repair', 'Brakes', '09:45:19');
-/*!40000 ALTER TABLE `car_maintenance_history` ENABLE KEYS */;
-
--- Dumping structure for table 332.comment
-CREATE TABLE IF NOT EXISTS `comment` (
-  `member_num` int(20) NOT NULL,
-  `vin` varchar(20) NOT NULL,
-  `rating` enum('1','2','3','4') NOT NULL,
-  `comment_text` varchar(100) NOT NULL,
-  `comment_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`member_num`,`vin`,`comment_time`),
-  KEY `FK_comment_car` (`vin`),
-  KEY `FK_comment_member` (`member_num`),
-  CONSTRAINT `FK_comment_car` FOREIGN KEY (`vin`) REFERENCES `car` (`vin`),
-  CONSTRAINT `FK_comment_member` FOREIGN KEY (`member_num`) REFERENCES `member` (`member_num`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Dumping data for table 332.comment: ~2 rows (approximately)
-/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
-INSERT INTO `comment` (`member_num`, `vin`, `rating`, `comment_text`, `comment_time`) VALUES
-	(123456, '123gh45rt7y6534rw', '3', 'Had a great time!', '2017-03-02 00:13:57'),
-	(654321, '736hfyst618hye76t', '4', 'Great ride!', '2017-03-02 00:13:18');
-/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
-
--- Dumping structure for table 332.member
-CREATE TABLE IF NOT EXISTS `member` (
-  `member_num` int(20) NOT NULL AUTO_INCREMENT,
-  `f_name` varchar(50) NOT NULL,
-  `l_name` varchar(50) NOT NULL,
-  `phone_num` varchar(10) NOT NULL,
-  `email` varchar(50) DEFAULT NULL,
-  `license_num` varchar(20) NOT NULL,
-  `annual_mem_fee` decimal(15,2) NOT NULL,
-  `role` enum('User','Admin') NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  PRIMARY KEY (`member_num`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Dumping data for table 332.member: ~2 rows (approximately)
-/*!40000 ALTER TABLE `member` DISABLE KEYS */;
-INSERT INTO `member` (`member_num`, `f_name`, `l_name`, `phone_num`, `email`, `license_num`, `annual_mem_fee`, `role`, `username`, `password`) VALUES
-	(123456, 'Mitch', 'Mulder', '905-756-73', NULL, 'L4762-73672-37262', 100.25, 'User', 'MitchMulder', 'rVs7ucMHad1i8V'),
-	(654321, 'Michael', 'Tanel', '905-362-37', 'michael.tanel@queensu.ca', 'M6352-72653-74653', 100.25, 'Admin', 'MichaelTanel', 'uThQkusSKCtym4');
-/*!40000 ALTER TABLE `member` ENABLE KEYS */;
-
--- Dumping structure for table 332.parking_location
-CREATE TABLE IF NOT EXISTS `parking_location` (
-  `parking_address` varchar(50) NOT NULL,
-  `num_spaces` int(10) NOT NULL,
-  PRIMARY KEY (`parking_address`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Dumping data for table 332.parking_location: ~1 rows (approximately)
-/*!40000 ALTER TABLE `parking_location` DISABLE KEYS */;
-INSERT INTO `parking_location` (`parking_address`, `num_spaces`) VALUES
-	('20 Division St', 100),
-	('67 Albert St', 55);
-/*!40000 ALTER TABLE `parking_location` ENABLE KEYS */;
-
--- Dumping structure for table 332.rental_history
-CREATE TABLE IF NOT EXISTS `rental_history` (
-  `vin` varchar(20) NOT NULL,
-  `member_num` int(20) NOT NULL,
-  `pick-up_odometer_reading` int(20) NOT NULL,
-  `drop-off_odometer_reading` int(20) NOT NULL,
-  `status` enum('Normal','Damaged','Not Running') NOT NULL,
-  `rent_date` date NOT NULL,
-  `pick-up_time` time NOT NULL,
-  `drop-off_time` time NOT NULL,
-  PRIMARY KEY (`rent_date`,`vin`,`member_num`),
-  KEY `FK_member_rental_history_member` (`member_num`),
-  KEY `FK_rental_history_car` (`vin`),
-  CONSTRAINT `FK_member_rental_history_member` FOREIGN KEY (`member_num`) REFERENCES `member` (`member_num`),
-  CONSTRAINT `FK_rental_history_car` FOREIGN KEY (`vin`) REFERENCES `car` (`vin`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- Dumping data for table 332.rental_history: ~2 rows (approximately)
-/*!40000 ALTER TABLE `rental_history` DISABLE KEYS */;
-INSERT INTO `rental_history` (`vin`, `member_num`, `pick-up_odometer_reading`, `drop-off_odometer_reading`, `status`, `rent_date`, `pick-up_time`, `drop-off_time`) VALUES
-	('123gh45rt7y6534rw', 123456, 34576, 37645, 'Normal', '2017-03-02', '00:00:00', '13:12:13'),
-	('736hfyst618hye76t', 654321, 3245, 7654, 'Normal', '2017-03-02', '08:56:17', '12:34:19');
-/*!40000 ALTER TABLE `rental_history` ENABLE KEYS */;
-
--- Dumping structure for table 332.reservation
-CREATE TABLE IF NOT EXISTS `reservation` (
-  `reservation_num` int(11) NOT NULL AUTO_INCREMENT,
-  `member_num` int(20) NOT NULL,
-  `vin` varchar(20) NOT NULL,
-  `reservation_date` date NOT NULL,
-  `access_code` int(20) NOT NULL,
-  `reservation_length` int(5) NOT NULL,
-  PRIMARY KEY (`reservation_num`),
-  KEY `FK_reservation_car` (`vin`),
-  KEY `FK_reservation_member` (`member_num`),
-  CONSTRAINT `FK_reservation_car` FOREIGN KEY (`vin`) REFERENCES `car` (`vin`),
-  CONSTRAINT `FK_reservation_member` FOREIGN KEY (`member_num`) REFERENCES `member` (`member_num`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
--- Dumping data for table 332.reservation: ~2 rows (approximately)
-/*!40000 ALTER TABLE `reservation` DISABLE KEYS */;
-INSERT INTO `reservation` (`reservation_num`, `member_num`, `vin`, `reservation_date`, `access_code`, `reservation_length`) VALUES
-	(1, 654321, '736hfyst618hye76t', '2017-03-02', 1234, 2),
-	(2, 123456, '123gh45rt7y6534rw', '2017-03-02', 432176, 4);
-/*!40000 ALTER TABLE `reservation` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+Updatecheck<|||>3<|||>1
+DoUsageStatistics<|||>3<|||>1
+UpdatecheckLastrun<|||>1<|||>2017-03-28 23:00:16
+LastUsageStatisticCall<|||>1<|||>2017-03-01 22:12:21
+LastSessions<|||>1<|||>TEST332
+LastActiveSession<|||>1<|||>TEST332
+ColWidths_connform.ListSessions<|||>1<|||>163,50,50,50,50,50,53
+ColsVisible_connform.ListSessions<|||>1<|||>0
+ColPositions_connform.ListSessions<|||>1<|||>0,1,2,3,4,5,6
+ColSort_connform.ListSessions<|||>1<|||>0,0
+ColWidths_UserManagerForm.listUsers<|||>1<|||>96,80
+ColsVisible_UserManagerForm.listUsers<|||>1<|||>0,1
+ColPositions_UserManagerForm.listUsers<|||>1<|||>0,1
+ColSort_UserManagerForm.listUsers<|||>1<|||>0,0
+ColWidths_frmTableEditor.listColumns<|||>1<|||>54,244,156,90,134,81,50,119,130,140,100,100
+ColsVisible_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11
+ColPositions_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11
+ColSort_frmTableEditor.listColumns<|||>1<|||>-1,0
+ColWidths_frmTableEditor.treeIndexes<|||>1<|||>341,100,80
+ColsVisible_frmTableEditor.treeIndexes<|||>1<|||>0,1,2
+ColPositions_frmTableEditor.treeIndexes<|||>1<|||>0,1,2
+ColSort_frmTableEditor.treeIndexes<|||>1<|||>-1,0
+ColWidths_frmTableEditor.listForeignKeys<|||>1<|||>99,80,100,80,80,80
+ColsVisible_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
+ColPositions_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
+ColSort_frmTableEditor.listForeignKeys<|||>1<|||>-1,0
+CoolBand0Index<|||>3<|||>0
+CoolBand0Break<|||>3<|||>1
+CoolBand0Width<|||>3<|||>1360
+CoolBand1Index<|||>3<|||>1
+CoolBand1Break<|||>3<|||>1
+CoolBand1Width<|||>3<|||>1360
+MainWinOnMonitor<|||>3<|||>0
+MainWinLeft<|||>3<|||>676
+MainWinTop<|||>3<|||>0
+MainWinWidth<|||>3<|||>697
+MainWinHeight<|||>3<|||>735
+ColWidths_MainForm.ListDatabases<|||>1<|||>150,80,50,50,50,50,50,50,50,50,120
+ColsVisible_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
+ColPositions_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
+ColSort_MainForm.ListDatabases<|||>1<|||>0,0
+ColWidths_MainForm.ListVariables<|||>1<|||>160,200,275
+ColsVisible_MainForm.ListVariables<|||>1<|||>0,1,2
+ColPositions_MainForm.ListVariables<|||>1<|||>0,1,2
+ColSort_MainForm.ListVariables<|||>1<|||>0,0
+ColWidths_MainForm.ListStatus<|||>1<|||>160,275,100,100
+ColsVisible_MainForm.ListStatus<|||>1<|||>0,1,2,3
+ColPositions_MainForm.ListStatus<|||>1<|||>0,1,2,3
+ColSort_MainForm.ListStatus<|||>1<|||>0,0
+ColWidths_MainForm.ListProcesses<|||>1<|||>70,80,80,80,80,50,50,145
+ColsVisible_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
+ColPositions_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
+ColSort_MainForm.ListProcesses<|||>1<|||>0,1
+ColWidths_MainForm.ListCommandStats<|||>1<|||>120,100,100,100,215
+ColsVisible_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
+ColPositions_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
+ColSort_MainForm.ListCommandStats<|||>1<|||>1,1
+ColWidths_MainForm.ListTables<|||>1<|||>120,70,70,120,120,70,100,50,70,70,70,70,70,90,120,70,70,70,50
+ColsVisible_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,18
+ColPositions_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
+ColSort_MainForm.ListTables<|||>1<|||>0,0
+DateTimeEditor_CursorPos_Type4<|||>3<|||>5
+FileDialogEncoding_loaddataform<|||>3<|||>0
+loadfilename<|||>1<|||>C:\Users\Michael\OneDrive\Documents\Queen's\3rd Year\Winter\CMPE 332\Assignment 1\Works_On Table.txt
+CSVSeparatorV2<|||>1<|||>\t
+CSVEncloserV2<|||>1<|||>
+CSVImportFieldEscaperV2<|||>1<|||>
+CSVImportDuplicateHandling<|||>3<|||>0
+TableEditorTabsHeight<|||>3<|||>90
+querymemoheight<|||>3<|||>390
+dbtreewidth<|||>3<|||>266
+MainWinMaximized<|||>3<|||>1
+ExportSQL_CreateDatabases<|||>3<|||>1
+ExportSQL_CreateTables<|||>3<|||>1
+ExportSQL_Output<|||>3<|||>0
+ExportSQL_ZipFilenames<|||>1<|||>C:\Users\Michael\OneDrive\Documents\Queen's\3rd Year\Winter\CMPE 332\Assignment 1\CreateTables.zip<{{{><}}}>C:\Users\Michael\Desktop\tomsaccess.zip<{{{><}}}>
+queryhelperswidth<|||>3<|||>66
+ExportSQL_DataHow<|||>3<|||>2
+ExportSQL_Filenames<|||>1<|||>C:\Users\Michael\Documents\GitHub\KTCS\app\332.sql<{{{><}}}>C:\Users\Michael\OneDrive\Documents\Queen's\3rd Year\Winter\CMPE 332\Assignment 1\CreateTablesWithData.sql<{{{><}}}>
+FileDialogEncoding_MainForm<|||>3<|||>0
+sqloutheight<|||>3<|||>65
+MemoEditorWidth<|||>3<|||>136
+SessionManager_WindowWidth<|||>3<|||>695
+SessionManager_WindowHeight<|||>3<|||>451
+SQLFile0<|||>1<|||>C:\xampp\htdocs\KTCS\app\332.sql
+SQLFile1<|||>1<|||>C:\Users\Michael\Documents\GitHub\KTCS\app\332.sql
+Servers\TEST332\SessionCreated<|||>1<|||>2017-03-29 00:41:41
+Servers\TEST332\Host<|||>1<|||>127.0.0.1
+Servers\TEST332\WindowsAuth<|||>3<|||>0
+Servers\TEST332\User<|||>1<|||>root
+Servers\TEST332\Password<|||>1<|||>2
+Servers\TEST332\LoginPrompt<|||>3<|||>0
+Servers\TEST332\Port<|||>1<|||>3306
+Servers\TEST332\NetType<|||>3<|||>0
+Servers\TEST332\Compressed<|||>3<|||>0
+Servers\TEST332\LocalTimeZone<|||>3<|||>0
+Servers\TEST332\QueryTimeout<|||>3<|||>0
+Servers\TEST332\KeepAlive<|||>3<|||>0
+Servers\TEST332\FullTableStatus<|||>3<|||>1
+Servers\TEST332\Databases<|||>1<|||>
+Servers\TEST332\Comment<|||>1<|||>
+Servers\TEST332\StartupScriptFilename<|||>1<|||>
+Servers\TEST332\SSHtunnelHost<|||>1<|||>
+Servers\TEST332\SSHtunnelHostPort<|||>3<|||>0
+Servers\TEST332\SSHtunnelUser<|||>1<|||>
+Servers\TEST332\SSHtunnelPassword<|||>1<|||>5
+Servers\TEST332\SSHtunnelTimeout<|||>3<|||>4
+Servers\TEST332\SSHtunnelPrivateKey<|||>1<|||>
+Servers\TEST332\SSHtunnelPort<|||>3<|||>3307
+Servers\TEST332\SSL_Active<|||>3<|||>0
+Servers\TEST332\SSL_Key<|||>1<|||>
+Servers\TEST332\SSL_Cert<|||>1<|||>
+Servers\TEST332\SSL_CA<|||>1<|||>
+Servers\TEST332\SSL_Cipher<|||>1<|||>
+Servers\TEST332\ServerVersionFull<|||>1<|||>10.1.19-MariaDB - mariadb.org binary distribution
+Servers\TEST332\ConnectCount<|||>3<|||>6
+Servers\TEST332\ServerVersion<|||>3<|||>100119
+Servers\TEST332\LastConnect<|||>1<|||>2017-03-29 22:42:53
+Servers\TEST332\lastUsedDB<|||>1<|||>332
+Servers\TEST332\332|reservation\Sort<|||>1<|||>0_reservation_length|
