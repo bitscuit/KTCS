d8cf26a9f27711493c836788b3efcf5ec741c6c4
Mitchell-Mulder <mitch.mulder88@gmail.com>
2017-04-01 12:29:38 -0400

    fixed up all the controllers

--- app/controllers/car_controller.php
+++ app/controllers/car_controller.php
@@ -1,62 +1,51 @@
 <?php
 class CarController {
     public function getViewAvailableCars() {
-		if (isset($_SESSION["signIn"]) && $_SESSION["signIn"] == 1) {
-
-			if (isset($_POST["carsOnDate"])) {
-				$date = $_POST["carsOnDate"];
-				echo $date;
-				$list = Car::getAvailableCars($date);
-					echo "<table>";
+		if (isset($_POST["carsOnDate"])) {
+			$date = $_POST["carsOnDate"];
+			echo $date;
+			$list = Car::getAvailableCars($date);
+				echo "<table>";
+				echo "<tr>";
+				echo "<th>Make</th>";
+				echo "<th>Model</th>";
+				echo "<th>Year</th>";
+				echo "</tr>";
+			if (!empty($list)) {
+				foreach ($list as $row) {
 					echo "<tr>";
-					echo "<th>Make</th>";
-					echo "<th>Model</th>";
-					echo "<th>Year</th>";
+					echo "<td>" . $row["make"] . "</td>";
+					echo "<td>" . $row["model"] . "</td>";
+					echo "<td>" . $row["make_year"] . "</td>";
 					echo "</tr>";
-				if (!empty($list)) {
-					foreach ($list as $row) {
-						echo "<tr>";
-						echo "<td>" . $row["make"] . "</td>";
-						echo "<td>" . $row["model"] . "</td>";
-						echo "<td>" . $row["make_year"] . "</td>";
-						echo "</tr>";
-					}
-				} else {
-					echo "No available cars on this date";
 				}
-				echo "</table>";
+			} else {
+				echo "No available cars on this date";
 			}
-			// get view to show list of available cars
-			require_once("views/car/available_cars.php");
-		} else {
-			header("Location: ?controller=error&action=getViewError");
-			exit;
+			echo "</table>";
 		}
+		// get view to show list of available cars
+		require_once("views/car/available_cars.php");
     }
 
     public function getViewLocationCars() {
-        if (isset($_SESSION["signIn"]) && $_SESSION["signIn"] == 1) {
-            if (isset($_GET["location"])) {
-                $location = $_GET["location"];
-                $list = Car::getLocationCars($location);
-                if (!empty($list)) {
-                    foreach ($list as $row) {
-                        echo "<tr>";
-                        echo "<td>" . $row['make'] . "</td>";
-                        echo "<td>" . $row['model'] . "</td>";
-                        echo "<td>" . $row['make_year'] . "</td>";
-                        echo "</tr>";
-                    }
+        if (isset($_GET["location"])) {
+            $location = $_GET["location"];
+            $list = Car::getLocationCars($location);
+            if (!empty($list)) {
+                foreach ($list as $row) {
+                    echo "<tr>";
+                    echo "<td>" . $row['make'] . "</td>";
+                    echo "<td>" . $row['model'] . "</td>";
+                    echo "<td>" . $row['make_year'] . "</td>";
+                    echo "</tr>";
                 }
-                echo "</table>";
-            } else {
-                echo "No available cars on this date";
             }
-            require_once("views/car/available_cars.php");
+            echo "</table>";
         } else {
-			header("Location: ?controller=error&action=getViewError");
-			exit;
+            echo "No available cars on this date";
         }
+        require_once("views/car/available_cars.php");
     }
 }
 ?>
--- app/controllers/location_controller.php
+++ app/controllers/location_controller.php
@@ -1,19 +1,14 @@
 <?php
 class LocationController {
     public function getViewLocation() {
-        if (isset($_SESSION["signIn"]) && $_SESSION["signIn"] == 1) {
-            $list = Location::all();
-            foreach ($list as $row) {
-                echo "<tr>";
-                echo "<td class='tableData'><a href='?controller=car&action=getViewLocationCars&location=" . $row['parking_address'] . "'>" . $row['parking_address'] . "</a></td>";
-                echo "</tr>";
-            }
-            echo "</table>";
-            require_once('views/location/location.php');
-        } else {
-            header("Location: ?controller=error&action=getViewError");
-			exit;
+        $list = Location::all();
+        foreach ($list as $row) {
+            echo "<tr>";
+            echo "<td class='tableData'><a href='?controller=car&action=getViewLocationCars&location=" . $row['parking_address'] . "'>" . $row['parking_address'] . "</a></td>";
+            echo "</tr>";
         }
+        echo "</table>";
+        require_once('views/location/location.php');
     }
 }
 ?>
