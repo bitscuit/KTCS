abf28c364a64b4013ef2d1a36655d09dda7ff248
Mitchell-Mulder <mitch.mulder88@gmail.com>
2017-04-05 14:47:44 -0400

    added better member and admin home pages

--- app/controllers/home_controller.php
+++ app/controllers/home_controller.php
@@ -1,9 +1,17 @@
 <?php
 	class HomeController {
 		public function getViewHome() {
-			$first_name = "Oprah";
-			$last_name  = "Winfrey";
-			require_once("views/home/home.php");
+			if (isset($_SESSION["signIn"]) && $_SESSION["signIn"] == 1 ) {
+				header("Location: ?controller=user&action=getViewMember");
+				exit;
+			} else if (isset($_SESSION["adminSignIn"]) && $_SESSION["adminSignIn"] == 1 ) {
+				header("Location: ?controller=admin&action=getViewAdmin");
+				exit;
+			} else {
+				$first_name = "Oprah";
+				$last_name  = "Winfrey";
+				require_once("views/home/home.php");
+			}
 		}
 	}
 ?>
