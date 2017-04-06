b2ea63c1ff169003d5b0618594449e83cda6b66c
NewmanM <m_tanel@live.ca>
2017-04-05 20:24:37 -0400

    fixed home page

--- app/views/member/member.php
+++ app/views/member/member.php
@@ -1,13 +1,16 @@
-<table>
-    <tr>
-        <th>Vin</th>
-        <th>Reservation Start Date</th>
-        <th>Reservation End Date</th>
-        <th>Access Code</th>
-    </tr>
-    <?php
+<?php
     if (!empty($reservations)) {
-        foreach ($reservations as $row) {
+        echo "<table class='table table-bordered'>";
+		echo "<thead>";
+		echo "<tr>";
+        echo "<th>Vin</th>";
+        echo "<th>Reservation Start Date</th>";
+        echo "<th>Reservation End Date</th>";
+        echo "<th>Access Code</th>";
+		echo "</tr>";
+		echo "</thead>";
+		echo "<tbody>";
+		foreach ($reservations as $row) {
             echo "<tr>";
             echo "<td>" . $row["vin"] . "</td>";
             echo "<td>" . $row["reservation_start_date"] . "</td>";
@@ -15,6 +18,7 @@
             echo "<td>" . $row["access_code"] . "</td>";
             echo "</tr>";
         }
+		echo "</tbody>";
+		echo "</table>";
     }
     ?>
-</table>
